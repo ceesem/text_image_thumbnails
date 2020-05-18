79a5f75e86656ea630fc359ab924940ddeb23db2
Casey Schneider-Mizell <caseysm@gmail.com>
2020-05-17 15:16:44 -1000

    improvements and click version

---
+++ make_thumbnails.py
@@ -0,0 +1,73 @@
+from src.thumbnail_maker import thumbnail_image, simple_filename, make_author_string
+import pandas as pd
+import click
+import datetime
+import os
+import tqdm
+import re
+
+TITLE_COLUMN = "title"
+ABSTRACT_COLUMN = "abstract"
+AUTHOR_COLUMN_CONTAINS = "author"
+THUMBNAIL_DIRECTORY = "thumbnail_images"
+MIN_HEIGHT = 570
+WIDTH = 1000
+
+
+@click.command()
+@click.option("--filename", "-f")
+@click.option("--min_height", "-h", default=MIN_HEIGHT)
+@click.option("--width", "-w", default=WIDTH)
+@click.option("--title_column", "-T", default=TITLE_COLUMN)
+@click.option("--abstract_column", "-A", default=ABSTRACT_COLUMN)
+@click.option("--author_column_contains", "-au", default=AUTHOR_COLUMN_CONTAINS)
+@click.option("--thumbnail_directory", "-d", default=THUMBNAIL_DIRECTORY)
+def generate_thumbnails(
+    filename,
+    min_height,
+    width,
+    title_column,
+    abstract_column,
+    author_column_contains,
+    thumbnail_directory,
+):
+    data = pd.read_csv(filename)
+
+    author_columns = []
+    for c in data.columns:
+        if re.match(author_column_contains, c) is not None:
+            author_columns.append(c)
+
+    author_list = []
+    for _, row in data[author_columns].iterrows():
+        auths = row[~pd.isna(row)].tolist()
+        author_list.append(make_author_string(auths))
+
+    title_list = data[title_column].tolist()
+    abstract_list = data[abstract_column].tolist()
+
+    if not os.path.exists(thumbnail_directory):
+        os.mkdir(thumbnail_directory)
+
+    batch_dir = f"batch_{str(datetime.date.today()).replace('-', '_')}"
+    if not os.path.exists(f"{thumbnail_directory}/{batch_dir}"):
+        os.mkdir(f"{thumbnail_directory}/{batch_dir}")
+
+    for title, authors, abstract in tqdm.tqdm(
+        zip(title_list, author_list, abstract_list), total=len(title_list)
+    ):
+        img = thumbnail_image(
+            title, authors, abstract, image_width=width, min_height=min_height
+        )
+        fname = simple_filename(
+            title, f"{thumbnail_directory}/{batch_dir}", max_words=8
+        )
+        img.save(
+            fname, dpi=(150, 150),
+        )
+
+    return
+
+
+if __name__ == "__main__":
+    generate_thumbnails()
--- src/thumbnail_maker.py
+++ src/thumbnail_maker.py
@@ -4,24 +4,46 @@ import textwrap
 from PIL import Image, ImageDraw, ImageFont
 
 try:
-    FONT_DIRS = {'library': '/Library/Fonts/',
-                 'user': os.path.expanduser('~/Library/Fonts/'),
-                 'system': '/System/Library/Fonts/'}
+    FONT_DIRS = {
+        "library": "/Library/Fonts/",
+        "user": os.path.expanduser("~/Library/Fonts/"),
+        "system": "/System/Library/Fonts/",
+    }
 except:
     pass
 
-DEFAULT_FONT = 'Avenir.ttc'
-DEFAULT_FONT_LOCATION = FONT_DIRS['system']
+DEFAULT_FONT = "Avenir.ttc"
+DEFAULT_FONT_LOCATION = FONT_DIRS["system"]
 
 
 def simple_filename(title, img_dir, max_words=10):
-    title = re.sub('[^A-Za-z0-9\s]+', '', title.lower())
-    title_l = title.split(' ')
-    title_s = '_'.join(title_l[0:max_words])
-    return f"{img_dir}{title_s}.png"
+    title = re.sub("[^A-Za-z0-9\s]+", "", title.lower())
+    title_l = title.split(" ")
+    title_s = "_".join(title_l[0:max_words])
+    return f"{img_dir}/{title_s}.png"
 
 
-def build_fonts(font_name, font_location, size=18, title_size=20, title_index=2, author_index=1, regular_index=0):
+def make_author_string(author_list, use_oxford=True):
+    if len(author_list) == 1:
+        return author_list[0]
+    if len(author_list) == 2:
+        use_oxford = False
+    author_string_front = ", ".join(author_list[:-1])
+    author_string = (
+        f"{author_string_front}{',' if use_oxford else ''} and {author_list[-1]}"
+    )
+    return author_string
+
+
+def build_fonts(
+    font_name,
+    font_location,
+    size=18,
+    title_size=20,
+    title_index=2,
+    author_index=1,
+    regular_index=0,
+):
     """Get image fonts for each section
 
     Parameters
@@ -48,28 +70,34 @@ def build_fonts(font_name, font_location, size=18, title_size=20, title_index=2,
         title font, author font, and text font
     """
     font_loc = f"{font_location}{font_name}"
-    fnt_title = ImageFont.truetype(font_loc, size=title_size, index=title_index, encoding='utf-8')
-    fnt_author = ImageFont.truetype(font_loc, size=size, index=author_index, encoding='utf-8')
-    fnt_text = ImageFont.truetype(font_loc, size=size, index=regular_index, encoding='utf-8')
+    fnt_title = ImageFont.truetype(
+        font_loc, size=title_size, index=title_index, encoding="utf-8"
+    )
+    fnt_author = ImageFont.truetype(
+        font_loc, size=size, index=author_index, encoding="utf-8"
+    )
+    fnt_text = ImageFont.truetype(
+        font_loc, size=size, index=regular_index, encoding="utf-8"
+    )
     return fnt_title, fnt_author, fnt_text
 
 
 def join_words(line):
-    return ' '.join(line)
+    return " ".join(line)
 
 
 def split_lines(txt, fnt, width, padding):
     w = width - 2 * padding
 
-    split_text = txt.split(' ')
+    split_text = txt.split(" ")
     lines = []
     new_line = []
     for word in split_text:
-        try_new_line = fnt.getsize(join_words(new_line + [word] + [' ']))
+        try_new_line = fnt.getsize(join_words(new_line + [word] + [" "]))
         if try_new_line[0] <= w:
             new_line.append(word)
         else:
-            lines.append(join_words(new_line) + '\n')
+            lines.append(join_words(new_line) + "\n")
             new_line = [word]
     lines.append(join_words(new_line))
     return lines
@@ -77,7 +105,7 @@ def split_lines(txt, fnt, width, padding):
 
 def centered_box_corner(lines, fnt, width):
     bbox_width = fnt.getsize_multiline(join_words(lines))[0]
-    right_corner = width / 2 - bbox_width/2
+    right_corner = width / 2 - bbox_width / 2
     return right_corner
 
 
@@ -93,7 +121,9 @@ def image_height(text_fonts, v_padding, p_spacing):
     return h, pts
 
 
-def assemble_image(text_font_ctr_spacing, width, w_padding, v_padding, section_spacing):
+def assemble_image(
+    text_font_ctr_spacing, width, w_padding, v_padding, section_spacing, min_height
+):
     lines_each = []
     fnts_each = []
     x_pts = []
@@ -101,7 +131,7 @@ def assemble_image(text_font_ctr_spacing, width, w_padding, v_padding, section_s
     spacing_each = []
     for text, fnt, is_centered, spacing in text_font_ctr_spacing:
         lines = split_lines(text, fnt, width, w_padding)
-        lines_each.append(''.join(lines))
+        lines_each.append("".join(lines))
         fnts_each.append(fnt)
         centered_each.append(is_centered)
         if is_centered:
@@ -112,35 +142,45 @@ def assemble_image(text_font_ctr_spacing, width, w_padding, v_padding, section_s
         spacing_each.append(spacing)
 
     text_fonts = [(l, f, s) for l, f, s in zip(lines_each, fnts_each, spacing_each)]
-    h_all, h_each = image_height(text_fonts,
-                                 v_padding,
-                                 section_spacing,
-                                 )
+    h_all, h_each = image_height(text_fonts, v_padding, section_spacing,)
+    if min_height is not None:
+        h_all = max([h_all, min_height])
 
-    img = Image.new('RGB', size=(width, h_all), color=(255, 255, 255))
+    img = Image.new("RGB", size=(width, h_all), color=(255, 255, 255))
     d = ImageDraw.Draw(img)
-    align_lookup = {True: 'center', False: 'left'}
-    for line, fnt, is_centered, spacing, x, y in zip(lines_each, fnts_each, centered_each, spacing_each, x_pts, h_each):
-        d.multiline_text((x, y), ''.join(line), font=fnt,
-                         align=align_lookup[is_centered], fill=(0, 0, 0), spacing=spacing)
+    align_lookup = {True: "center", False: "left"}
+    for line, fnt, is_centered, spacing, x, y in zip(
+        lines_each, fnts_each, centered_each, spacing_each, x_pts, h_each
+    ):
+        d.multiline_text(
+            (x, y),
+            "".join(line),
+            font=fnt,
+            align=align_lookup[is_centered],
+            fill=(0, 0, 0),
+            spacing=spacing,
+        )
     return img
 
 
-def thumbnail_image(title,
-                    authors,
-                    abstract,
-                    font_name=DEFAULT_FONT,
-                    font_location=DEFAULT_FONT_LOCATION,
-                    font_size=18,
-                    title_size=20,
-                    title_font_style=2,
-                    author_font_style=1,
-                    text_font_style=0,
-                    line_spacing=10,
-                    image_width=1000,
-                    horizontal_padding=50,
-                    vertical_padding=50,
-                    section_spacing=25):
+def thumbnail_image(
+    title,
+    authors,
+    abstract,
+    font_name=DEFAULT_FONT,
+    font_location=DEFAULT_FONT_LOCATION,
+    font_size=18,
+    title_size=20,
+    title_font_style=2,
+    author_font_style=1,
+    text_font_style=0,
+    line_spacing=10,
+    image_width=1000,
+    horizontal_padding=50,
+    vertical_padding=50,
+    section_spacing=25,
+    min_height=None,
+):
     """Generate a thumbnail image of specified text.
 
     Parameters
@@ -173,25 +213,39 @@ def thumbnail_image(title,
         Horizontal margins in pixels, by default 50
     vertical_padding : int, optional
         Vertical margins in pixels, by default 50
-    section_spacing : int, jjkkoptional
+    section_spacing : int, optional
         Spacing between sections in pixels, by default 25
+    min_height : int or None, optional
+        Minimum total image height, default is None.
 
     Returns
     -------
     PIL.Image
     """
-    title_font, author_font, abstract_font = build_fonts(font_name,
-                                                         font_location,
-                                                         size=font_size,
-                                                         title_size=title_size,
-                                                         title_index=title_font_style,
-                                                         author_index=author_font_style,
-                                                         regular_index=text_font_style)
-    tfc = [(text_cleanup(title), title_font, True, line_spacing),
-           (text_cleanup(authors), author_font, True, line_spacing),
-           (text_cleanup(abstract), abstract_font, False, line_spacing)]
-    return assemble_image(tfc, image_width, horizontal_padding, vertical_padding, section_spacing)
+    title_font, author_font, abstract_font = build_fonts(
+        font_name,
+        font_location,
+        size=font_size,
+        title_size=title_size,
+        title_index=title_font_style,
+        author_index=author_font_style,
+        regular_index=text_font_style,
+    )
+    tfc = [
+        (text_cleanup(title), title_font, True, line_spacing),
+        (text_cleanup(authors), author_font, True, line_spacing),
+        (text_cleanup(abstract), abstract_font, False, line_spacing),
+    ]
+    return assemble_image(
+        tfc,
+        image_width,
+        horizontal_padding,
+        vertical_padding,
+        section_spacing,
+        min_height,
+    )
+
 
 def text_cleanup(text):
-    text = re.sub('‐','-', text)
+    text = re.sub("‐", "-", text)
     return text
